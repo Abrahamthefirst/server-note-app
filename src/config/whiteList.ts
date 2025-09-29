c4971e0915f3d028683a0bc3516884b86f5c481c
Abrahamthefirst <abrahamodusegun36@gmail.com>
2025-09-29 12:17:17 +0100

    vercel frontend url

--- src/config/whiteList.ts
+++ src/config/whiteList.ts
@@ -3,4 +3,6 @@ export const whitelist = [
   "http://localhost:3500",
   "http://localhost:3001",
   "http://localhost:3000/auth/google",
+  "https://note-app-ts-optimization.vercel.app/",
+  "https://client-note-app.onrender.com",
 ];
